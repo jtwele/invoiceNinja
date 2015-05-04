b97ca13d5dd89f443b90c52c84e1ab520fd21f9f
Natalie <nata@hawai.de>
2015-05-04 13:25:55 +0200

    Testkommentar eingegeben

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,10 @@ use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel {
+	
+	/**
+	 * Dies ist ein Test!
+	 */
 
 	/**
 	 * The Artisan commands provided by your application.
