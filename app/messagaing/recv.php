389169b346c29d0b3ac008f866f6603b368ce1fb
jeremias.twele <jeremias@twele.net>
2015-05-26 10:28:46 +0200

    testausgabe get_clients()

--- app/messagaing/recv.php
+++ app/messagaing/recv.php
@@ -12,11 +12,11 @@ echo ' ** Waiting for messages. To exit press CTRL+C **', "\n";
 
 $callback = function ($msg) {
 
-    $find_client = false;
+    $find_client = true;
 
     $create_client = false;
     $create_invoice = false;
-    $get_clients = true;
+    $get_clients = false;
     $get_invoices = false;
 
     if($create_client){
