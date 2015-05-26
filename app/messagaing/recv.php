9cd4ca0cbda369d87c2c02bdf603687f45d265d8
jeremias.twele <jeremias@twele.net>
2015-05-26 14:18:41 +0200

    testausgabe email_invoice()

--- app/messagaing/recv.php
+++ app/messagaing/recv.php
@@ -195,7 +195,7 @@ function email_invoice(){
      * -H "X-Ninja-Token: TOKEN"
      */
     $data = array(
-        "id" => '21'
+        "id" => '16'
     );
 
     $data_string = json_encode($data);
