d4f7f86af289a6937c58e915a385fbe1651b9072
jeremias.twele <jeremias@twele.net>
2015-05-26 10:17:51 +0200

    testausgabe get_clients()

--- app/messagaing/recv.php
+++ app/messagaing/recv.php
@@ -149,7 +149,8 @@ function get_invoices(){
 
     $invoice_url = 'localhost/api/v1/invoices';
     $ch = curl_init($invoice_url);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Ninja-Token: GuTtJU276mbWvAQnpFrw0ylvkRkaq6H6', CURLOPT_RETURNTRANSFER => 1));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Ninja-Token: GuTtJU276mbWvAQnpFrw0ylvkRkaq6H6'));
     curl_setopt($ch, CURLOPT_HTTPGET, true);
     $output=curl_exec($ch);
     echo $output;
