2e36712a193cc82f0e3d599c39fbcf1c6433fb6f
jeremias.twele <jeremias@twele.net>
2015-05-26 09:59:34 +0200

    testausgabe get_clients()

--- app/messagaing/recv.php
+++ app/messagaing/recv.php
@@ -118,7 +118,7 @@ function create_invoice() {
 function find_client(){
     $clients = get_clients();
     echo "\n",'**************************************************', "\n";
-    printr($clients);
+    print_r($clients);
     echo "\n",'**************************************************';
     //:TODO client finden impln
 }
